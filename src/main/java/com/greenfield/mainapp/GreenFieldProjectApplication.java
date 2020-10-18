0db47fd2bacd0d30258bcb745060ec9440bd2bb1
markrubio632 <mark.rubio.632@gmail.com>
2020-10-18 10:59:08 -0700

    added simple microservice capability

--- src/main/java/com/greenfield/mainapp/GreenFieldProjectApplication.java
+++ src/main/java/com/greenfield/mainapp/GreenFieldProjectApplication.java
@@ -2,6 +2,7 @@ package com.greenfield.mainapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -10,6 +11,7 @@ import com.greenfield.repository.EnrolleeRepo;
 @SpringBootApplication(scanBasePackages = "com.greenfield")
 @EnableMongoRepositories(basePackageClasses = EnrolleeRepo.class)
 @ComponentScan(basePackages = "com.greenfield")
+@EnableEurekaServer
 public class GreenFieldProjectApplication {
 
 	public static void main(String[] args) {
